958ed71814f82e2ada57300ca9835b638af413aa
adeliabraguta <adeliabraguta@gmail.com>
2025-11-28 17:38:03 +0200

    First Commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,8 +5,7 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
+  standalone: true,
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('movies');
-}
+export class App {}
