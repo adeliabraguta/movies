a90c1a0ebb8260044b4cdba260eaf86e63c9b4a9
adeliabraguta <adeliabraguta@gmail.com>
2025-12-01 22:13:41 +0200

    Add dark mode

--- src/app/pages/movies-page/movies-page.component.ts
+++ src/app/pages/movies-page/movies-page.component.ts
@@ -1,13 +1,27 @@
-import { Component, signal } from '@angular/core';
+import { Component, effect, signal } from '@angular/core';
 import { SearchMoviesComponent } from '../../components/search-movies/search-movies.component';
 import { MovieListComponent } from '../../components/movie-list/movie-list.component';
+import { IconMoonComponent } from '../../shared/ui/icons/icon-moon/icon-moon.component';
+import { IconSunComponent } from '../../shared/ui/icons/icon-sun/icon-sun.component';
 
 @Component({
   selector: 'app-movies-page',
-  imports: [SearchMoviesComponent, MovieListComponent],
+  imports: [SearchMoviesComponent, MovieListComponent, IconMoonComponent, IconSunComponent],
   templateUrl: './movies-page.component.html',
-  standalone: true
+  standalone: true,
 })
 export class MoviesPageComponent {
   searchQuery = signal<string>('');
+  darkMode = signal(false);
+
+  constructor() {
+    effect(() => {
+      const isDark = this.darkMode();
+      document.documentElement.classList.toggle('dark', isDark);
+    });
+  }
+
+  toggleDarkMode() {
+    this.darkMode.update((dark) => !dark);
+  }
 }
