8dc89445866b3605b48cd0ba3a080bde94d6e203
adeliabraguta <adeliabraguta@gmail.com>
2025-12-01 18:39:42 +0200

    Add configs

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,19 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes)],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(
+      routes,
+      withInMemoryScrolling({
+        scrollPositionRestoration: 'top',
+      }),
+    ),
+    provideHttpClient(withInterceptors([authInterceptor])),
+  ],
 };
